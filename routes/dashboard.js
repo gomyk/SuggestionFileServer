1ccb4f0ae1de298f6d5f2b0b1afc7362fc465bce
moonwon kim <gomyk@naver.com>
2019-12-20 13:59:13 +0900

    Add dashboard views

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,5 +7,31 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+router.get('/index', function(req, res, next) {
+  res.render('index.html');
+});
 
+router.get('/index2', function(req, res, next) {
+  res.render('index2.html');
+});
+
+router.get('/index3', function(req, res, next) {
+  res.render('index3.html');
+});
+
+router.get('/index3-horizontal', function(req, res, next) {
+  res.render('index3-horizontalmenu.html');
+});
+
+router.get('/barchart', function(req, res, next) {
+  res.render('barchart.html');
+});
+
+router.get('/linechart', function(req, res, next) {
+  res.render('linechart.html');
+});
+
+router.get('/piechart', function(req, res, next) {
+  res.render('piechart.html');
+});
 module.exports = router;
